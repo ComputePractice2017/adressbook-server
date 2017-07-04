3f783429e708e0dc7ac694e27d957aeb1c4898eb
Ivan Belyavtsev <djbelyak@gmail.com>
2017-07-04 14:38:30 +0300

    Изменения прошлого стрима Related issue #14

--- api/server.go
+++ api/server.go
@@ -3,8 +3,10 @@ package api
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ComputePractice2017/adressbook-server/model"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -21,9 +23,16 @@ func Run() {
 	r.HandleFunc("/", helloWorldHandler).Methods("GET")
 	r.HandleFunc("/persons", getAllPersonsHandler).Methods("GET")
 	r.HandleFunc("/persons", newPersonHandler).Methods("POST")
+	r.HandleFunc("/persons", firstOptionsHandler).Methods("OPTIONS")
 	r.HandleFunc("/persons/{guid}", editPersonHandler).Methods("PUT")
 	r.HandleFunc("/persons/{guid}", deletePersonHandler).Methods("DELETE")
+	r.HandleFunc("/persons/{guid}", secondOptionsHandler).Methods("OPTIONS")
 
 	log.Println("Running the server on port 8000...")
-	http.ListenAndServe(":8000", r)
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	http.ListenAndServe(":8000", handlers.CORS(originsOk, headersOk, methodsOk)(r))
 }
